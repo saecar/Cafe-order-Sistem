b4a2f931e4eeec230ba7b339797766f649107e21
saecar <maybedills@gmail.com>
2026-09-07 08:32:40 +0700

    fix: correct vercel frontend domain in cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173',  'https://cafe-frontend-ltci.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173',  'https://cafe-frontend-ltci-beta.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
